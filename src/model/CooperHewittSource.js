c05d4e188b0347bfd9b99fc10b65243cee866f9c
Christoph Johns <christophjohns@gmail.com>
2021-03-31 13:20:26 +0200

    Add object to interact with CooperHewitt API

---
+++ src/model/CooperHewittSource.js
@@ -0,0 +1,39 @@
+// Object to interact with the Cooper Hewitt API
+const CooperHewittSource = {
+    // Method to get object collections from the API
+    // Params:
+    // - numberOfCollections: number of collections to be fetched (default: 10)
+    // Returns:
+    // Promise of array of collection objects
+    // Example collections array:
+    // [{
+    //     title: "Botanical Expressions",
+    //     images: [
+    //         {
+    //             id: "123456",
+    //             url: "https://...",
+    //             liked: false,
+    //         },
+    //     ]
+    // },]
+    getCollections(numberOfCollections = 10) {
+        const collectionsPromise = new Promise((resolve, reject) => { // Placeholder for actual API call
+            setTimeout(() => { 
+                const exampleCollections = [{
+                    title: "Botanical Expressions",
+                    images: [
+                        {
+                            id: "123456",
+                            url: "https://images.collection.cooperhewitt.org/152056_fd1b6ff7c925bf70_b.jpg",
+                            liked: false,
+                        },
+                    ]
+                },];
+                resolve(exampleCollections); // Return example array for now
+            }, 300);
+          });
+        return collectionsPromise;
+    },
+};
+
+export default CooperHewittSource;
