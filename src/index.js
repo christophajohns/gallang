28b9f5e578583be01868b114c28b7c7efc7263f3
Christoph Johns <christophjohns@gmail.com>
2021-03-31 13:22:30 +0200

    Fix GallangModel import

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import GallangModel from './GallangModel';
+import { GallangModel } from './model';
 
 const myModel = new GallangModel();
 
