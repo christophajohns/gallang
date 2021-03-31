c3a49911f181690b5e1725a3e8bc9ea364dba6d1
Christoph Johns <christophjohns@gmail.com>
2021-03-31 13:19:20 +0200

    Add HorizontalGrid, Image and promiseNoData to exports

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,11 @@
 import Button from './Button';
+import HorizontalGrid from './HorizontalGrid';
+import Image from './Image';
+import promiseNoData from './promiseNoData';
 
 export {
     Button,
+    HorizontalGrid,
+    Image,
+    promiseNoData,
 };
