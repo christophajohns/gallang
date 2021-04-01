3ff165f2ea95d9fac7d77b123f44f0d884cf95ac
Christoph Johns <christophjohns@gmail.com>
2021-04-01 11:01:55 +0200

    Add basic layout for HomeView

--- src/views/HomeView/HomeView.js
+++ src/views/HomeView/HomeView.js
@@ -15,24 +15,25 @@ function HomeView(props) {
     return (
         <div className="HomeView">
             <div>This would be a carousel.</div>
-            {collections ? collections.map(collection => (
-                <HorizontalGrid
-                    key={collection.title}
-                    title={collection.title}
-                    images={collection.images}
-                />))
-            : ""}
-            {recentlyViewedImages ? <HorizontalGrid title="Recently viewed" images={recentlyViewedImages} /> : ""}
-            {quote ? <p className="quote">{quote}</p> : ""}
-            {recommendations ? recommendations.map(recommendation => (
-                <HorizontalGrid
-                    key={recommendation.title}
-                    title={recommendation.title}
-                    description="Recommended for you."
-                    images={recommendation.images}
-                />))
-            : ""}
-
+            <main>
+                {collections ? collections.map(collection => (
+                    <HorizontalGrid
+                        key={collection.title}
+                        title={collection.title}
+                        images={collection.images}
+                    />))
+                : ""}
+                {recentlyViewedImages ? <HorizontalGrid title="Recently viewed" images={recentlyViewedImages} /> : ""}
+                {quote ? <p className="quote">{quote}</p> : ""}
+                {recommendations ? recommendations.map(recommendation => (
+                    <HorizontalGrid
+                        key={recommendation.title}
+                        title={recommendation.title}
+                        description="Recommended for you."
+                        images={recommendation.images}
+                    />))
+                : ""}
+            </main>
         </div>
     );
 }
