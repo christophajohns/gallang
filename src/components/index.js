1bdb82acbc2a634c12b18f7edf054768f9a87b59
Christoph Johns <christophjohns@gmail.com>
2021-04-02 14:37:38 +0200

    Remove unused Button component

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,5 @@
-import Button from './Button';
-import HorizontalGrid from './HorizontalGrid';
-import Image from './Image';
-import promiseNoData from './promiseNoData';
+import HorizontalGrid from "./HorizontalGrid";
+import Image from "./Image";
+import promiseNoData from "./promiseNoData";
 
-export {
-    Button,
-    HorizontalGrid,
-    Image,
-    promiseNoData,
-};
+export { HorizontalGrid, Image, promiseNoData };
