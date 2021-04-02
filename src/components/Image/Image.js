1df16c59a19419938480a9d60f816b7dda810074
Christoph Johns <christophjohns@gmail.com>
2021-04-02 14:44:22 +0200

    Add JSDoc comments

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -1,14 +1,21 @@
-import './Image.css';
-import { Button } from 'react-bootstrap';
+import "./Image.css";
+import { Button } from "react-bootstrap";
 import {
     Download,
     GripVertical,
     Heart,
     HeartFill,
-} from 'react-bootstrap-icons';
-import PropTypes from 'prop-types';
+} from "react-bootstrap-icons";
+import PropTypes from "prop-types";
 
-/** Single object (image) showcase including like and download buttons */
+/**
+ * Single object (image) showcase including like and download buttons
+ * @param {Object} props
+ * @param {string} props.id - Unique identifier of the object and thereby image
+ * @param {string} props.src - Image url for the object image
+ * @param {boolean} props.liked - Flag whether the user has liked this image
+ * @returns
+ */
 function Image(props) {
     const {
         id, // Specify the title to placed on top of the image grid
@@ -19,10 +26,16 @@ function Image(props) {
     return (
         <div className="Image">
             <img id={id} src={src} alt={id} />
-            <Button variant="link" className="grip-button"><GripVertical /></Button>
+            <Button variant="link" className="grip-button">
+                <GripVertical />
+            </Button>
             <div className="Image__buttons">
-                <Button variant="link" className="like-button">{liked ? <HeartFill /> : <Heart />}</Button>
-                <Button variant="link" className="download-button"><Download /></Button>
+                <Button variant="link" className="like-button">
+                    {liked ? <HeartFill /> : <Heart />}
+                </Button>
+                <Button variant="link" className="download-button">
+                    <Download />
+                </Button>
             </div>
         </div>
     );
@@ -35,6 +48,6 @@ Image.propTypes = {
     src: PropTypes.string.isRequired,
     /** Flag whether the user has liked this image */
     liked: PropTypes.bool.isRequired,
-}
+};
 
 export default Image;
