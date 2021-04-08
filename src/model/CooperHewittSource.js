d7df39f67cb93dfea320947516f0fc8234da2361
Christoph Johns <christophjohns@gmail.com>
2021-04-08 14:50:07 +0200

    Add apiCall method

--- src/model/CooperHewittSource.js
+++ src/model/CooperHewittSource.js
@@ -4,6 +4,24 @@ import "../types";
 /** Object to interact with the Cooper Hewitt API */
 const CooperHewittSource = {
     /**
+     * Make a call to the Cooper Hewitt API
+     * @param {string} params - String representing the API method's parameters
+     * @returns Response as a JavaScript object
+     */
+    async apiCall(params) {
+        try {
+            const response = await fetch(`${process.env.BASE_URL}${params}`, {
+                method: "GET",
+            });
+            if (response.status !== 200) {
+                throw response.statusText;
+            }
+            return response.json();
+        } catch (error) {
+            console.log("Error:", error);
+        }
+    },
+    /**
 	 * Get object collections from the API.
 	 * @param {number} numberOfCollections - Number of collections to be fetched (default: 10; compare items per page in API Docs)
 	 * @returns {Promise<Collection[]>} - Promise object holding an array of collections
