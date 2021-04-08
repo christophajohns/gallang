a97d139ceab71c29ed49262e73443709535344e0
Christoph Johns <christophjohns@gmail.com>
2021-04-08 14:43:11 +0200

    Import and configure dotenv

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import dotenv from "dotenv";
 
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { GallangModel } from './model';
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { GallangModel } from "./model";
 
+// Configure dotenv (keys and values available at process.env)
+dotenv.config();
+
+// Initialize model
 const myModel = new GallangModel();
 
-ReactDOM.render(
-  <App model={myModel} />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App model={myModel} />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
