f31d5bc645c64e4f7c39c398c9cef2510f601755
Christoph Johns <christophjohns@gmail.com>
2021-04-08 15:21:49 +0200

    Add method to search objects

--- src/model/CooperHewittSource.js
+++ src/model/CooperHewittSource.js
@@ -5,12 +5,19 @@ import "../types";
 const CooperHewittSource = {
     /**
      * Make a call to the Cooper Hewitt API
-     * @param {string} params - String representing the API method's parameters
-     * @returns Response as a JavaScript object
+     * @param {Object} params - String representing the API method's parameters
+     * @param {string} params.method - API method to call
+     * @returns {Promise<Object>} - Promise holding the response as a JavaScript object
      */
     async apiCall(params) {
+        const filteredParams = filterTruthyKeys(params);
+        const urlSearchParams = new URLSearchParams({
+            access_token: process.env.ACCESS_TOKEN,
+            ...filteredParams,
+        });
+        const url = `${process.env.BASE_URL}?${urlSearchParams}`;
         try {
-            const response = await fetch(`${process.env.BASE_URL}${params}`, {
+            const response = await fetch(url, {
                 method: "GET",
             });
             if (response.status !== 200) {
@@ -20,6 +27,27 @@ const CooperHewittSource = {
         } catch (error) {
             console.log("Error:", error);
         }
+    },
+    /**
+     * Search objects in the Cooper Hewitt collection (see https://collection.cooperhewitt.org/api/methods/cooperhewitt.search.objects)
+     * @param {Object} searchParams - Object representing of the search parameters
+     * @param {string} searchParams.query - Search for objects matching terms across all the text fields
+     * @param {number} maximumNumberOfResults - Maximum number of objects to return (i.e. objects per page since only one page is returned; maximum 500)
+     * @returns {Promise<CooperHewittObject[]>} - Array holding objects with information about one Cooper Hewitt Object each matching the search parameters
+     */
+    async searchObjects(searchParams, maximumNumberOfResults = 12) {
+        if (maximumNumberOfResults > 500) {
+            throw Error("Maximum 500 objects can be returned on one page.");
+        }
+        const params = {
+            method: "cooperhewitt.search.objects",
+            has_images: true,
+            page: 1,
+            per_page: maximumNumberOfResults,
+            ...searchParams,
+        };
+        const data = await CooperHewittSource.apiCall(params);
+        return data.objects;
     },
     /**
 	 * Get object collections from the API.
@@ -67,5 +95,19 @@ const CooperHewittSource = {
         return quotePromise;
     },
 };
+
+// -- Utility functions --
+/**
+ * Utility function to filter and keep only truthy keys from an object
+ * @param {Object} object - Object to filter
+ * @returns {Object} - Object containing only truthy keys from the original object
+ */
+function filterTruthyKeys(object) {
+    const filteredObject = {};
+    Object.keys(object).forEach((key, _index) => {
+        if (object[key]) filteredObject[key] = object[key];
+    });
+    return filteredObject;
+}
 
 export default CooperHewittSource;
