7992742d132765c3f3840d0e87390bc88f306e5b
Christoph Johns <christophjohns@gmail.com>
2021-04-09 11:38:13 +0200

    Centralize propType definitions

--- src/components/VerticalGrid/VerticalGrid.js
+++ src/components/VerticalGrid/VerticalGrid.js
@@ -1,8 +1,19 @@
+import PropTypes from "prop-types";
+import { imageType } from "../../types";
+
 /**
  * Vertical (scrollable) grid of images to showcase objects in a the results view
+ * @param {Object} props - Properties to be passed to the component
+ * @param {Image[]} props.images - Array of images to render in the grid
  */
-function VerticalGrid() {
+function VerticalGrid(props) {
+    const { images } = props;
+
     return <div>Here should be a vertical grid.</div>;
 }
+
+VerticalGrid.propTypes = {
+    images: PropTypes.arrayOf(imageType),
+};
 
 export default VerticalGrid;
--- src/types/index.js
+++ src/types/index.js
@@ -1,1 +1,2 @@
-export * from "./JSDocTypes";
+export * from "./jsDocTypes";
+export * from "./propTypes";
