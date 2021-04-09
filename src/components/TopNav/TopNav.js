6843a4a758a4fe4e29e6dbd195e435b019c9a660
Christoph Johns <christophjohns@gmail.com>
2021-04-09 15:20:16 +0200

    Use most basic version of static top nav

--- src/components/TopNav/TopNav.js
+++ src/components/TopNav/TopNav.js
@@ -1,76 +1,15 @@
-import React from "react";
-import { CSSTransition } from "react-transition-group";
-import * as Icon from "react-bootstrap-icons";
-import {
-    HeaderWrapper,
-    Logo,
-    SearchWrapper,
-    NavSearch,
-    AccountWrapper,
-    Account,
-    AccountOptionsWrapper,
-    Option,
-    LogoWrapper,
-    ControlsWrapper,
-} from "./style";
+import { NavBar, Brand, ControlsDiv } from "./style";
 
-function TopNav(props) {
-    const {
-        focused,
-        mouseIn,
-        options,
-        onSearch,
-        handleChange,
-        handleInputFocus,
-        handleInputBlur,
-        handleAccountMouseEnter,
-        handleAccountMouseLeave,
-    } = props;
-
+/** Navigation bar for the whole application */
+function TopNav() {
     return (
-        <div>
-            <HeaderWrapper>
-                <LogoWrapper>
-                    <Logo>Gallang</Logo>
-                </LogoWrapper>
-
-                <ControlsWrapper>
-                    <SearchWrapper>
-                        {/* <CSSTransition
-                    in={focused}
-                    timeout={200}
-                    className="slide"
-                > */}
-                        <form onSubmit={onSearch}>
-                            <NavSearch
-                                // className={focused ? 'focused' : ''}
-                                // onFocus={handleInputFocus}
-                                onBlur={handleInputBlur}
-                                onChange={(e) => handleChange(e.target.value)}
-                            />
-                            <Icon.Search></Icon.Search>
-                        </form>
-
-                        {/* </CSSTransition> */}
-                    </SearchWrapper>
-                    <AccountWrapper onMouseEnter={handleAccountMouseEnter}>
-                        <Account>G</Account>
-                        {
-                            <AccountOptionsWrapper
-                                // show = {mouseIn}
-                                onMouseLeave={handleAccountMouseLeave}
-                            >
-                                {options.map((option) => {
-                                    return (
-                                        <Option key={option}>{option}</Option>
-                                    );
-                                })}
-                            </AccountOptionsWrapper>
-                        }
-                    </AccountWrapper>
-                </ControlsWrapper>
-            </HeaderWrapper>
-        </div>
+        <NavBar sticky={false}>
+            <Brand href="/">Gallang</Brand>
+            <ControlsDiv>
+                <div>Search bar</div>
+                <div>User</div>
+            </ControlsDiv>
+        </NavBar>
     );
 }
 
--- src/presenters/TopNavPresenter.js
+++ src/presenters/TopNavPresenter.js
@@ -1,45 +1,11 @@
-import React from "react";
 import { TopNav } from "../components";
-import { promiseNoData } from "../components";
-import { CooperHewittSource } from "../model";
-import { usePromise } from "./customHooks";
 
-function TopNavPresenter(props) {
-    // State
-    const [focused, setFocused] = React.useState(false);
-    const [mouseIn, setMouseIn] = React.useState(false);
-    const [options, setOptions] = React.useState(["Account", "Logout"]);
-    const [searchInput, setSearchInput] = React.useState("");
-    const [promise, setPromise] = React.useState(null);
-
-    // Effects
-    React.useEffect(() => {
-        setPromise(CooperHewittSource.searchObjects({}));
-    }, []);
-
-    const [data, setData, error, setError] = usePromise(promise);
-
-    return (
-        <div>
-            <TopNav
-                focused={focused}
-                mouseIn={mouseIn}
-                options={options}
-                onSearch={(e) => {
-                    setData("");
-                    setPromise(CooperHewittSource.searchObjects(searchInput));
-                    e.preventDefault();
-                }}
-                handleChange={(txt) => setSearchInput(txt)}
-                handleInputFocus={() => setFocused(true)}
-                handleInputBlur={() => setFocused(false)}
-                handleAccountMouseEnter={() => setMouseIn(true)}
-                handleAccountMouseLeave={() => setMouseIn(false)}
-            />
-            {promiseNoData(promise, data, error) ||
-                props.model.saveResults(data)}
-        </div>
-    );
+/**
+ * Presenter for the TopNav component
+ * @returns TopNav component
+ */
+function TopNavPresenter() {
+    return <TopNav />;
 }
 
 export default TopNavPresenter;
