8fac56a2a382600392a96101ae239c2a82ce5092
Christoph Johns <christophjohns@gmail.com>
2021-04-09 11:08:43 +0200

    Add and render results view

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import './App.css';
-import {
-  HomePresenter,
-} from './presenters';
+import "./App.css";
+import { HomePresenter } from "./presenters";
+import { ResultsView } from "./views";
 
 function App(props) {
-  const {
-    model, // Model keeping application state
-  } = props;
+    const {
+        model, // Model keeping application state
+    } = props;
 
-  return (
-    <div className="App">
-      <HomePresenter model={model} />
-    </div>
-  );
+    return (
+        <div className="App">
+            <ResultsView />
+            {/* <HomePresenter model={model} /> */}
+        </div>
+    );
 }
 
 export default App;
---
+++ src/views/ResultsView/ResultsView.js
@@ -0,0 +1,12 @@
+/**
+ * View component for the Results (e.g. search results, collection, liked content, gallery) page content.
+ */
+function ResultsView() {
+    return (
+        <main className="ResultsView">
+            <div>Here should be a results view.</div>
+        </main>
+    );
+}
+
+export default ResultsView;
