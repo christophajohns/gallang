f980f83bf6cff31a8a71b8854860859811d3283b
Christoph Johns <christophjohns@gmail.com>
2021-04-11 15:47:58 +0200

    Add placeholder search results presenter

---
+++ src/presenters/SearchResultsPresenter.js
@@ -0,0 +1,8 @@
+import { ResultsView } from "../views";
+
+/** Placeholder presenter to test routing for the search results */
+function SearchResultsPresenter() {
+    return <ResultsView contentType={"search results"} />;
+}
+
+export default SearchResultsPresenter;
--- src/presenters/index.js
+++ src/presenters/index.js
@@ -1,5 +1,11 @@
 import HomePresenter from "./HomePresenter";
 import HorizontalGridPresenter from "./HorizontalGridPresenter";
 import ImagePresenter from "./ImagePresenter";
+import SearchResultsPresenter from "./SearchResultsPresenter";
 
-export { HomePresenter, HorizontalGridPresenter, ImagePresenter };
+export {
+    HomePresenter,
+    HorizontalGridPresenter,
+    ImagePresenter,
+    SearchResultsPresenter,
+};
