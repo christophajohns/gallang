057df6fb06a2f400cb91dde885b4d3ef28994076
Christoph Johns <christophjohns@gmail.com>
2021-04-11 16:09:31 +0200

    Add basic routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import './App.css';
-import {
-  HomePresenter,
-} from './presenters';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import { HomePresenter, SearchResultsPresenter } from "./presenters";
 
 function App(props) {
-  const {
-    model, // Model keeping application state
-  } = props;
+    const {
+        model, // Model keeping application state
+    } = props;
 
-  return (
-    <div className="App">
-      <HomePresenter model={model} />
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Switch>
+                    <Route path="/search">
+                        <SearchResultsPresenter />
+                    </Route>
+                    <Route path="/">
+                        <HomePresenter model={model} />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
--- src/presenters/SearchResultsPresenter.js
+++ src/presenters/SearchResultsPresenter.js
@@ -1,7 +1,21 @@
+import { useURLSearchParams } from "./customHooks";
 import { ResultsView } from "../views";
 
 /** Placeholder presenter to test routing for the search results */
 function SearchResultsPresenter() {
+    const urlSearchParams = useURLSearchParams();
+
+    /**
+     * Example function on how to access information from the URL
+     * @param {URLSearchParams} urlSearchParams - URLSearchParams object representing the parameters passed via the URL
+     */
+    function logQueryStringFromURL(urlSearchParams) {
+        const query = urlSearchParams.get("query");
+        console.log({ query });
+    }
+
+    logQueryStringFromURL(urlSearchParams);
+
     return <ResultsView contentType={"search results"} />;
 }
 
