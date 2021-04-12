7bd14c003cd83aa5043cb8ea57efad3cdbb83d40
Christoph Johns <christophjohns@gmail.com>
2021-04-12 12:41:49 +0200

    Use imageID from URL params

--- src/presenters/DetailsPresenter.js
+++ src/presenters/DetailsPresenter.js
@@ -1,8 +1,11 @@
+import { useParams } from "react-router-dom";
 import { DetailsView } from "../views";
 
 /** Placeholder presenter to test routing for the details view */
 function DetailsPresenter() {
-    return <DetailsView imageID="12345" />;
+    const { imageID } = useParams();
+
+    return <DetailsView imageID={imageID} />;
 }
 
 export default DetailsPresenter;
