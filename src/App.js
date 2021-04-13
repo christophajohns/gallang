1b3006d9f621684bd53a83f247164eae63946b43
Christoph Johns <christophjohns@gmail.com>
2021-04-13 09:31:53 +0200

    Fix render of search results view

--- src/App.js
+++ src/App.js
@@ -1,43 +1,40 @@
-
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import {
     DetailsPresenter,
     HomePresenter,
     SearchResultsPresenter,
-    TopNavPresenter, 
+    TopNavPresenter,
     LikedContentPresenter,
     CollectionPresenter,
     GalleryPresenter,
 } from "./presenters";
 import { mockCollections } from "./model/MockData";
 
-
 function App(props) {
     const {
         model, // Model keeping application state
     } = props;
 
-
     return (
-        <Router>
-            <div className="App">
+        <div className="App">
+            <TopNavPresenter model={model} />
+            <Router>
                 <Switch>
                     <Route path="/search">
-                        <SearchResultsPresenter />
+                        <SearchResultsPresenter model={model} />
                     </Route>
 
                     <Route path="/details/:imageID">
-                        <DetailsPresenter />
+                        <DetailsPresenter model={model} />
                     </Route>
 
                     <Route path="/">
                         <HomePresenter model={model} />
                     </Route>
                 </Switch>
-            </div>
-        </Router>
-
+            </Router>
+        </div>
     );
 }
 
--- src/presenters/SearchResultsPresenter.js
+++ src/presenters/SearchResultsPresenter.js
@@ -1,25 +1,41 @@
-
+import React from "react";
 import PropTypes from "prop-types";
 import { useURLSearchParams } from "./customHooks";
 import { ResultsPresenter } from "../presenters";
+import { promiseNoData } from "../components";
+import { usePromise } from "./customHooks";
 import { imageType } from "../types";
 import { modelType as imagePresenterModelType } from "./ImagePresenter";
+import { CooperHewittSource } from "../model";
 
 /**
  * Presenter for the search results page content.
  * @param {Object} props - Properties to be passed to the view
- * @param {string} props.searchQuery - Search query string the user has searched for
- * @param {number} props.numberOfResults - Total number of results returned
- * @param {Image[]} props.images - Array of images to render in the grid
  * @param {Object} props.model - Model keeping the application state
  * @param {Function} props.model.likeImage - Function to like an image by its ID
  * @param {Function} props.model.unlikeImage - Function to unlike an image by its ID
  * @param {string[]} props.model.likedImageIDs - Array of image IDs the user has liked already
- * @param {URLSearchParams} urlSearchParams - URLSearchParams object representing the parameters passed via the URL 
-*/
+ */
 function SearchResultsPresenter(props) {
-    const { searchQuery, numberOfResults, images, model } = props;
+    // Props
+    const { model } = props;
+
+    // State
+    const [searchPromise, setSearchPromise] = React.useState(null);
+    const searchPromiseStatesAndSetters = usePromise(searchPromise);
+    const searchData = searchPromiseStatesAndSetters[0];
+    const searchError = searchPromiseStatesAndSetters[2];
+
+    // Custom Hooks
     const urlSearchParams = useURLSearchParams();
+    const query = urlSearchParams.get("query");
+
+    // Effects
+    React.useEffect(() => {
+        // only at creation
+        setSearchPromise(CooperHewittSource.searchObjects({ query }));
+    }, [query]);
+
     function logQueryStringFromURL(urlSearchParams) {
         const query = urlSearchParams.get("query");
         console.log({ query });
@@ -27,14 +43,19 @@ function SearchResultsPresenter(props) {
 
     logQueryStringFromURL(urlSearchParams);
     return (
-        <ResultsPresenter
-            contentType="search results"
-            title={`"${searchQuery}"`}
-            numberOfObjects={numberOfResults}
-            images={images}
-            allowDownloadAll={false}
-            model={model}
-        />
+        promiseNoData(searchPromise, searchData, searchError) || (
+            <ResultsPresenter
+                contentType="search results"
+                title={`"${query}"`}
+                numberOfObjects={searchData.length}
+                images={searchData.map((object) => ({
+                    id: object.id,
+                    url: object.images[0].b.url,
+                }))}
+                allowDownloadAll={false}
+                model={model}
+            />
+        )
     );
 }
 
@@ -44,6 +65,5 @@ SearchResultsPresenter.propTypes = {
     images: PropTypes.arrayOf(imageType).isRequired,
     model: imagePresenterModelType.isRequired,
 };
-
 
 export default SearchResultsPresenter;
