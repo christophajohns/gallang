db91509812485cfc37b0f2000409fb1c8139593c
Christoph Johns <christophjohns@gmail.com>
2021-04-13 12:47:14 +0200

    Add type definition file under correct name

---
+++ src/types/jsDocTypes.js
@@ -0,0 +1,27 @@
+// This file only serves to hold the JSDoc type definitions in one place.
+
+/**
+ * @typedef Collection
+ * @property {string} title - Name or title of the collection
+ * @property {string} id - Unique identifier of the collection
+ * @property {number} numberOfImages - The number of images or objects within the collection/exhibition
+ * @property {Image[]} images - Array of objects or images within the collection
+ */
+
+/**
+ * @typedef Recommendation
+ * @property {string} title - Name or title for the recommendation basis (e.g. medium, period, designer)
+ * @property {Image[]} images - Array of objects or images that are being recommended
+ * @property {string} basisImageID - Identifier of the image that formed the basis for why these images were recommended
+ */
+
+/**
+ * @typedef Image
+ * @property {string} id - Unique identifier of the object or image
+ * @property {string} url - Image url for the object
+ */
+
+/**
+ * Function to be called when a model property changes.
+ * @callback observerCallback
+ */
