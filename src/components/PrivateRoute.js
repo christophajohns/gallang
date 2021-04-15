0fd0168e07cd9605b2239b505c907826748eeab0
Christoph Johns <christophjohns@gmail.com>
2021-04-15 18:58:54 +0200

    Add private route

---
+++ src/components/PrivateRoute.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { AuthenticationModel } from "../model";
+
+/**
+ * Component to only route to specified component if the user is currently logged in
+ * @param {Object} props
+ * @param {Function} props.children - Nodes passed as children to the component
+ * @returns Passed component or redirect to login page
+ */
+function PrivateRoute(props) {
+    const {
+        children,
+        ...rest // other props passed to the route
+    } = props;
+
+    return (
+        <Route {...rest}>
+            {AuthenticationModel.currentUser ? (
+                children
+            ) : (
+                <Redirect to="/login" />
+            )}
+        </Route>
+    );
+}
+
+export default PrivateRoute;
--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,14 @@ import HorizontalGrid from "./HorizontalGrid";
 import Image from "./Image";
 import VerticalGrid from "./VerticalGrid";
 import TopNav from "./TopNav";
+import PrivateRoute from "./PrivateRoute";
 import promiseNoData from "./promiseNoData";
 
-export { HorizontalGrid, Image, VerticalGrid, promiseNoData, TopNav };
+export {
+    HorizontalGrid,
+    Image,
+    VerticalGrid,
+    promiseNoData,
+    TopNav,
+    PrivateRoute,
+};
