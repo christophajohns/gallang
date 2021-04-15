5e037d462ea98253809005a8b7110042de8940b2
Christoph Johns <christophjohns@gmail.com>
2021-04-15 18:18:45 +0200

    Handle login requests

--- src/presenters/LoginPresenter.js
+++ src/presenters/LoginPresenter.js
@@ -1,11 +1,49 @@
+import React from "react";
 import { LoginView } from "../views";
+import { AuthenticationModel } from "../model";
+import { useHistory } from "react-router";
 
 /**
  * Presenter for the login view
  * @returns Login view
  */
 function LoginPresenter() {
-    return <LoginView />;
+    const emailRef = React.useRef(null);
+    const passwordRef = React.useRef(null);
+
+    const [isLoading, setIsLoading] = React.useState(false);
+    const [loginError, setLoginError] = React.useState(null);
+
+    const browserHistory = useHistory();
+
+    async function loginUser(event) {
+        event.preventDefault(); // Do not reload page on submit
+
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+
+        try {
+            setIsLoading(true);
+            await AuthenticationModel.signInWithEmailAndPassword(
+                email,
+                password
+            );
+            browserHistory.push("/");
+        } catch (error) {
+            setLoginError(error);
+            setIsLoading(false);
+        }
+    }
+
+    return (
+        <LoginView
+            emailRef={emailRef}
+            passwordRef={passwordRef}
+            onRequestLogin={(e) => loginUser(e)}
+            isLoading={isLoading}
+            error={loginError?.message}
+        />
+    );
 }
 
 export default LoginPresenter;
