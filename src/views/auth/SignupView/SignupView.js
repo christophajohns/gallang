4c56f2460c9317f82410307b5d50c2184d603a1a
Christoph Johns <christophjohns@gmail.com>
2021-04-16 12:43:48 +0200

    Add props to handle signup

--- src/views/auth/SignupView/SignupView.js
+++ src/views/auth/SignupView/SignupView.js
@@ -1,5 +1,6 @@
 import { Lock } from "react-bootstrap-icons";
 import { Link } from "react-router-dom";
+import { Alert } from "react-bootstrap";
 import { GroupedInputs } from "./style";
 import AuthInputField from "../AuthInputField";
 import EmailInput from "../EmailInput";
@@ -8,17 +9,37 @@ import { CenterContentDiv, StyledForm, AuthButton } from "../style";
 
 /**
  * View component for the login page.
- */
-function SignupView() {
+ * @param {Object} props - Properties passed to the view
+ * @param {Function} props.onRequestSignup - Function to call when the signup form is submitted
+ * @param {React.MutableRefObject} props.emailRef - Reference to the email/username input field to handle signup request
+ * @param {React.MutableRefObject} props.passwordRef - Reference to the password input field to handle signup request
+ * @param {React.MutableRefObject} props.confirmPasswordRef - Reference to the confirm password input field to handle signup request
+ * @param {boolean} [props.isLoading = false] - Flag whether the user is currently being logged in
+ * @param {Error} props.error - Error from the login request */
+function SignupView(props) {
+    const {
+        onRequestSignup,
+        emailRef,
+        passwordRef,
+        confirmPasswordRef,
+        isLoading = false,
+        error,
+    } = props;
+
     return (
         <CenterContentDiv className="SignupView">
-            <StyledForm>
-                <EmailInput />
+            <StyledForm onSubmit={onRequestSignup}>
+                {error && <Alert variant="danger">{error}</Alert>}
+                <EmailInput emailRef={emailRef} />
                 <GroupedInputs>
-                    <PasswordInput />
-                    <ConfirmPasswordInput />
+                    <PasswordInput passwordRef={passwordRef} />
+                    <ConfirmPasswordInput
+                        confirmPasswordRef={confirmPasswordRef}
+                    />
                 </GroupedInputs>
-                <AuthButton type="submit">Create account</AuthButton>
+                <AuthButton disabled={isLoading} type="submit">
+                    {isLoading ? "Creating account…" : "Create account"}
+                </AuthButton>
                 <div>
                     Already have an account? <Link to="/login">Login</Link>
                 </div>
@@ -29,14 +50,19 @@ function SignupView() {
 
 /**
  * Component to render a the confirm password text input field
+ * @param {Object} props - Properties passed to the component
+ * @param {React.MutableRefObject} props.confirmPasswordRef - Reference to the confirm password input field to handle signup request
  */
-function ConfirmPasswordInput() {
+function ConfirmPasswordInput(props) {
+    const { confirmPasswordRef } = props;
+
     return (
         <AuthInputField
             icon={() => <Lock />}
             type="password"
             placeholder="Confirm password"
             name="confirm-password"
+            authRef={confirmPasswordRef}
         />
     );
 }
