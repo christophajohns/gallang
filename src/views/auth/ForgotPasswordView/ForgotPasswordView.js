fea09cf25e6ac34e2c14740d036e4b632aec29b6
Christoph Johns <christophjohns@gmail.com>
2021-04-16 13:58:00 +0200

    Add hooks to handle forgot password request

--- src/views/auth/ForgotPasswordView/ForgotPasswordView.js
+++ src/views/auth/ForgotPasswordView/ForgotPasswordView.js
@@ -1,25 +1,37 @@
 import { CenterContentDiv, AuthButton, StyledForm } from "../style";
 import { Link } from "react-router-dom";
 import EmailInput from "../EmailInput";
+import PropTypes from "prop-types";
+import { refType } from "../../../types";
 
 /**
  * View component for the forgot password page.
+ * @param {Object} props - Properties passed to the component
+ * @param {Function} props.onRequestPasswordReset - Function to be called when a user submits the password reset form
+ * @param {React.MutableRefObject} emailRef - Reference to the email/username input field to handle password reset request
  */
-function ForgotPasswordView() {
+function ForgotPasswordView(props) {
+    const { onRequestPasswordReset, emailRef } = props;
+
     return (
         <CenterContentDiv className="ForgotPasswordView">
-            <StyledForm>
+            <StyledForm onSubmit={onRequestPasswordReset}>
                 <p>
                     If you continue, Gallang will send a message to the e-mail
                     address specified below. Click the link in the message, and
                     enter a new password on the page that opens.
                 </p>
-                <EmailInput />
+                <EmailInput emailRef={emailRef} />
                 <AuthButton type="submit">Send password reset email</AuthButton>
                 <Link to="/login">Back to login</Link>
             </StyledForm>
         </CenterContentDiv>
     );
 }
+
+ForgotPasswordView.propTypes = {
+    onRequestPasswordReset: PropTypes.func.isRequired,
+    emailRef: refType.isRequired,
+};
 
 export default ForgotPasswordView;
