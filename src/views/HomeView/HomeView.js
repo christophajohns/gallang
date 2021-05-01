d09e1e5fa499af46c8b8480c6df443f249a12adc
Christoph Johns <christophjohns@gmail.com>
2021-05-01 15:24:08 +0200

    Don't pass model to home view

--- src/views/HomeView/HomeView.js
+++ src/views/HomeView/HomeView.js
@@ -1,5 +1,4 @@
 import PropTypes from "prop-types";
-import { HorizontalGridPresenter } from "../../presenters";
 import CollectionCarousel from "./CollectionCarousel";
 import Quote from "./Quote";
 import "../../types";
@@ -8,68 +7,38 @@ import { imageType } from "../../types";
 /**
  * View component for the Home/Browse page content.
  * @param {Object} props - Properties to be passed to the view
- * @param {Collection[]} props.collections - Array holding all information about collections to be rendered in a HorizontalGrid
- * @param {Image[]} props.recentlyViewedImages - Array of image data to be rendered in a HorizontalGrid
+ * @param {Function} props.collections - Component(s) to display featured collections
+ * @param {Collection[]} props.collectionsData - Array holding all information about collections to be rendered in a carousel
+ * @param {Function} [props.recentlyViewedImages] - Component(s) to display recently viewed images
  * @param {string} props.quote - String representing a quote
- * @param {Recommendation[]} props.recommendations - Array of recommended images and the recommendation basis (e.g. medium, period, designer)
- * @param {GallangModel} props.model - The model holding the application state
+ * @param {Function} props.recommendations - Component(s) to display recommended categories
  */
 function HomeView(props) {
     const {
-        collections, // Array holding all information about collections to be rendered in a HorizontalGrid
-        recentlyViewedImages, // Array of image data to be rendered in a HorizontalGrid
+        collections, // Component(s) to display featured collections
+        collectionsData, // Array holding all information about collections to be rendered in a carousel
+        recentlyViewedImages, // Component(s) to display recently viewed images
         quote, // String representing a quote
-        recommendations, // Array of recommended images and the recommendation basis (e.g. medium, period, designer)
-        model, // The model holding the application state
+        recommendations, // Component(s) to display recommended categories
     } = props;
-
-    const firstFourCollections = collections.slice(0, 4);
-    const collectionsAfterFour = collections.slice(4, 10);
 
     return (
         <div className="HomeView">
-            <CollectionCarousel collections={firstFourCollections} />
+            <CollectionCarousel collections={collectionsData.slice(0, 4)} />
+
             <main>
-                {recentlyViewedImages?.length ? ( // Only show if at least one recently viewed image
-                    <HorizontalGridPresenter
-                        title="Recently viewed"
-                        images={recentlyViewedImages}
-                        model={model}
-                    />
-                ) : (
-                    ""
-                )}
-                {collections
-                    ? collectionsAfterFour.map((collection) => (
-                          <HorizontalGridPresenter
-                              key={collection.title}
-                              type="collection"
-                              title={collection.title}
-                              images={collection.images}
-                              model={model}
-                          />
-                      ))
-                    : ""}
+                {recentlyViewedImages}
+                {collections}
                 {quote ? <Quote quoteText={quote} /> : ""}
-                {recommendations
-                    ? recommendations.map((recommendation) => (
-                          <HorizontalGridPresenter
-                              key={recommendation.title}
-                              title={recommendation.title}
-                              description="Recommended for you."
-                              images={recommendation.images}
-                              model={model}
-                          />
-                      ))
-                    : ""}
+                {recommendations}
             </main>
         </div>
     );
 }
 
 HomeView.propTypes = {
-    /** Array holding all information about collections to be rendered in a HorizontalGrid */
-    collections: PropTypes.arrayOf(
+    /** Array holding all information about collections to be rendered in a carousel */
+    collectionsData: PropTypes.arrayOf(
         PropTypes.shape({
             /** Name or title of the collection */
             title: PropTypes.string.isRequired,
@@ -77,25 +46,14 @@ HomeView.propTypes = {
             images: PropTypes.arrayOf(imageType).isRequired,
         })
     ),
-    /** Array of image data to be rendered in a HorizontalGrid */
-    recentlyViewedImages: PropTypes.arrayOf(imageType.isRequired),
+    /** Component(s) to display featured collections */
+    collections: PropTypes.node,
+    /** Component(s) to display recently viewed images */
+    recentlyViewedImages: PropTypes.node,
     /** String representing a quote */
     quote: PropTypes.string,
-    /** Array of recommended images and the recommendation basis (e.g. medium, period, designer) */
-    recommendations: PropTypes.arrayOf(
-        PropTypes.shape({
-            /** Name or title for the recommendation basis (e.g. medium, period, designer) */
-            title: PropTypes.string.isRequired,
-            /** Array of objects or images that are being recommended */
-            images: PropTypes.arrayOf(imageType.isRequired),
-        })
-    ),
-    /** The model holding the application state */
-    model: PropTypes.shape({
-        likedImageIDs: PropTypes.arrayOf(PropTypes.string).isRequired,
-        likeImage: PropTypes.func.isRequired,
-        unlikeImage: PropTypes.func.isRequired,
-    }),
+    /** Component(s) to display recommended categories */
+    recommendations: PropTypes.node.isRequired,
 };
 
 export default HomeView;
