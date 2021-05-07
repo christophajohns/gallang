78e39098158e398e7d1fc201f916c0e7ce6f3688
Christoph Johns <christophjohns@gmail.com>
2021-05-07 19:51:02 +0200

    Adding documenting comments

--- src/model/persistModel.js
+++ src/model/persistModel.js
@@ -1,60 +1,73 @@
-import { DatabaseModel, AuthenticationModel } from "./firebase";
+import { DatabaseModel } from "./firebase";
 
+/**
+ * Connects firebase and the GallangModel properties galleries, likedImageIDs and recentlyViewedImages
+ * @param {GallangModel} model - The model keeping the application state
+ */
 function persistModel(model) {
-    let loadingFromFirebase = false;
-    const currentUser = AuthenticationModel.currentUser;
-    var uid = "";
-    var currentUserName = "";
-    if (currentUser != null) {
-        uid = currentUser.uid; 
-        currentUserName = currentUser.displayName;
-    } else {
-        uid = "logout";
-        currentUserName = "";
-    }
-    const userRef = DatabaseModel.ref("gallang/"+uid);
+    let loadingFromFirebase = false; // Flag to show whether we are currently fetching data
+
+    // References via path to the firebase Realtime Database records
+    const userPath = `gallang/${model.currentUserID}`;
+    const userRef = DatabaseModel.ref(userPath);
     const galleriesRef = userRef.child("galleries");
     const likedImagesRef = userRef.child("likedImageIDs");
     const recentlyViewedImagesRef = userRef.child("recentlyViewedImages");
 
-    model.addObserver(function(){
-        if (loadingFromFirebase===true) return;
-        else {
-            userRef.set({
-                currentUser: currentUserName,
-                galleries: model.galleries,
-                likedImageIDs: model.likedImageIDs,
-                recentlyViewedImages: model.recentlyViewedImages,
-            });
-        }
-    });
-    console.log(currentUserName);
-    galleriesRef.on("value", snapshot => {
-        loadingFromFirebase = true;
-        const data = snapshot.val();
-        if(data) {
-            model.setGalleries(data);
-        }
+    // Update firebase whenever changes in the local model occur
+    model.addObserver(() => {
+        if (loadingFromFirebase === true) return;
+
+        // Update values on firebase
+        userRef.set({
+            currentUser: model.currentUserName,
+            galleries: model.galleries,
+            likedImageIDs: model.likedImageIDs,
+            recentlyViewedImages: model.recentlyViewedImages,
+        });
     });
 
-    likedImagesRef.on("value", snapshot => {
+    // Update galleries property in model when firebase value changes
+    galleriesRef.on("value", (snapshot) => {
         loadingFromFirebase = true;
-        const data = snapshot.val();
-        if (data) {
-            model.setLikedImageIDs(data);
-        }
-        loadingFromFirebase = false;
-    })
 
-    recentlyViewedImagesRef.on("value", snapshot => {
+        const galleriesFromFirebase = snapshot.val();
+        if (galleriesFromFirebase) {
+            const formattedGalleries = galleriesFromFirebase.map(
+                (currentGallery) => ({
+                    imageIDs: [], // gallery might not have imageIDs, property would be missing
+                    ...currentGallery,
+                })
+            );
+            model.galleries = formattedGalleries; // will notify observers
+        }
+
+        loadingFromFirebase = false;
+    });
+
+    // Update likedImageIDs property in model when firebase value changes
+    likedImagesRef.on("value", (snapshot) => {
         loadingFromFirebase = true;
-        const data = snapshot.val();
-        if (data) {
-            model.setRecentlyViewedImages(data);
-        }
-        loadingFromFirebase = false;
-    })
 
+        const likedImageIDsFromFirebase = snapshot.val();
+        if (likedImageIDsFromFirebase) {
+            model.likedImageIDs = likedImageIDsFromFirebase;
+        }
+
+        loadingFromFirebase = false;
+    });
+
+    // Update recentlyViewedImages property in model when firebase value changes
+    recentlyViewedImagesRef.on("value", (snapshot) => {
+        loadingFromFirebase = true;
+
+        const recentlyViewedImagesFromFirebase = snapshot.val();
+        if (recentlyViewedImagesFromFirebase) {
+            model.recentlyViewedImages = recentlyViewedImagesFromFirebase;
+        }
+
+        loadingFromFirebase = false;
+    });
 }
 
 export default persistModel;
