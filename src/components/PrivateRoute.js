1c061d09c9188158d2b5aa5e463a8623d8f4934d
Christoph Johns <christophjohns@gmail.com>
2021-05-08 12:52:10 +0200

    Use model instead of auth directly

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,26 +1,25 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
-import { AuthenticationService } from "../model";
+// eslint-disable-next-line no-unused-vars
+import { GallangModel } from "../model"; // only imported for JSDoc type
 
 /**
  * Component to only route to specified component if the user is currently logged in
  * @param {Object} props
+ * @param {GallangModel} props.model - Model keeping the application state
  * @param {Function} props.children - Nodes passed as children to the component
  * @returns Passed component or redirect to login page
  */
 function PrivateRoute(props) {
     const {
+        model,
         children,
         ...rest // other props passed to the route
     } = props;
 
     return (
         <Route {...rest}>
-            {AuthenticationService.currentUser ? (
-                children
-            ) : (
-                <Redirect to="/login" />
-            )}
+            {model.currentUser ? children : <Redirect to="/login" />}
         </Route>
     );
 }
