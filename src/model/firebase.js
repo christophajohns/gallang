eb0895c9da15c3c6384b4939ab8485bab5f987c7
Christoph Johns <christophjohns@gmail.com>
2021-05-17 12:44:30 +0200

    Log firebase config

--- src/model/firebase.js
+++ src/model/firebase.js
@@ -12,6 +12,7 @@ const firebaseConfig = {
     appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
+console.log({ firebaseConfig });
 firebase.initializeApp(firebaseConfig);
 
 export const DatabaseService = firebase.database();
