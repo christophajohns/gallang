7ca18100912d4b52e6d83dfca8d973561c44e538
Christoph Johns <christophjohns@gmail.com>
2021-05-17 12:49:53 +0200

    Remove firebase log

--- src/model/firebase.js
+++ src/model/firebase.js
@@ -12,7 +12,6 @@ const firebaseConfig = {
     appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
-console.log({ firebaseConfig });
 firebase.initializeApp(firebaseConfig);
 
 export const DatabaseService = firebase.database();
